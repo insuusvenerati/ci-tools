207bd2df952d4a6174b5b33acc6914bde9cb7b33
Sean Norwood <norwood.sean@gmail.com>
2021-10-12 17:50:36 -0500

    fix: update helm push script

--- scripts/helm-push.sh
+++ scripts/helm-push.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-VERSION=$(jq -r .version package.json)
+CHART_VERSION=$(yq eval '.version' k8s/treats-portfolio/Chart.yaml)
 
 git clone https://github.com/insuusvenerati/charts.git
 
-mv ${DRONE_REPO_NAME}-${VERSION}.tgz ./charts
+mv ${DRONE_REPO_NAME}-${CHART_VERSION}.tgz ./charts
 cd ./charts
-git add ${DRONE_REPO_NAME}-${VERSION}.tgz
-git commit -m chore:(${DRONE_REPO_NAME}) update app version to ${VERSION}
+git add ${DRONE_REPO_NAME}-${CHART_VERSION}.tgz
+git commit -m chore:(${DRONE_REPO_NAME}) update app version to ${CHART_VERSION}
