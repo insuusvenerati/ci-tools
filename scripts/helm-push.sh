75f7d235a1a05c31b345181508c3bb2499794991
Sean Norwood <norwood.sean@gmail.com>
2021-10-12 18:02:45 -0500

    fix: fix helm push script

--- scripts/helm-push.sh
+++ scripts/helm-push.sh
@@ -7,4 +7,4 @@ git clone https://github.com/insuusvenerati/charts.git
 mv ${DRONE_REPO_NAME}-${CHART_VERSION}.tgz ./charts
 cd ./charts
 git add ${DRONE_REPO_NAME}-${CHART_VERSION}.tgz
-git commit -m chore:(${DRONE_REPO_NAME}) update app version to ${CHART_VERSION}
+git commit -m chore:${DRONE_REPO_NAME} update app version to ${CHART_VERSION}
