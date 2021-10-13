a8879b2a7715dbf6478928b3537331512b91a398
Sean Norwood <norwood.sean@gmail.com>
2021-10-13 07:47:32 -0500

    chore: update helm script

--- scripts/helm-push.sh
+++ scripts/helm-push.sh
@@ -3,18 +3,19 @@
 CHART_VERSION=$(yq eval '.version' k8s/treats-portfolio/Chart.yaml)
 CHART_TARBALL="${DRONE_REPO_NAME}"-"${CHART_VERSION}".tgz
 
-echo "Chart version: ${CHART_VERSION}"
+echo "---Chart version: ${CHART_VERSION}---"
 
-echo "Cloning charts repo"
-git clone https://github.com/insuusvenerati/charts.git
+echo "---Cloning charts repo---"
+git clone git@github.com:insuusvenerati/charts.git
 
-echo "Moving ${CHART_TARBALL} to charts repo"
+echo "---Moving ${CHART_TARBALL} to charts repo---"
 mv "${CHART_TARBALL}" ./charts
 cd ./charts/docs || exit
 
+echo "---Updating repo index---"
 helm repo index docs --url https://insuusvenerati.github.io/charts/
 
-echo "Adding ${CHART_TARBALL} and commiting to repo"
+echo "---Adding ${CHART_TARBALL} and commiting to repo---"
 git add -A
 git commit -m "${DRONE_REPO_NAME} update app version to ${CHART_VERSION}"
 git push -u origin main
