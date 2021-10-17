721985704b4fb866d52d5faed2ab48f33a4cac89
Sean Norwood <norwood.sean@gmail.com>
2021-10-17 11:52:42 -0500

    chore: echo some information in ci

--- scripts/sentry-release.sh
+++ scripts/sentry-release.sh
@@ -3,6 +3,10 @@
 SENTRY_RELEASE=$DRONE_TAG
 SENTRY_ENVIRONMENT=$(/scripts/environment.sh)
 
+echo "---${SENTRY_RELEASE}---"
+echo "---${SENTRY_ENVIRONMENT}---"
+echo "---${SENTRY_PROJECT}---"
+
 sentry-cli --auth-token $SENTRY_AUTH_TOKEN releases new $SENTRY_RELEASE -p $SENTRY_PROJECT
 sentry-cli --auth-token $SENTRY_AUTH_TOKEN releases set-commits $SENTRY_RELEASE --auto
 sentry-cli --auth-token $SENTRY_AUTH_TOKEN releases files $SENTRY_RELEASE upload-sourcemaps ./public
