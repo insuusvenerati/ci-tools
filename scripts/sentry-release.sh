108dbd69ff49c64a36270f20dd6ec7394c3e0dbc
Sean Norwood <norwood.sean@gmail.com>
2021-10-17 11:51:43 -0500

    feat: add sentry release script

---
+++ scripts/sentry-release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SENTRY_RELEASE=$DRONE_TAG
+SENTRY_ENVIRONMENT=$(/scripts/environment.sh)
+
+sentry-cli --auth-token $SENTRY_AUTH_TOKEN releases new $SENTRY_RELEASE -p $SENTRY_PROJECT
+sentry-cli --auth-token $SENTRY_AUTH_TOKEN releases set-commits $SENTRY_RELEASE --auto
+sentry-cli --auth-token $SENTRY_AUTH_TOKEN releases files $SENTRY_RELEASE upload-sourcemaps ./public
+sentry-cli --auth-token $SENTRY_AUTH_TOKEN releases finalize $SENTRY_RELEASE
+sentry-cli --auth-token $SENTRY_AUTH_TOKEN releases deploys $SENTRY_RELEASE new -e $SENTRY_ENVIRONMENT
