c5bc943dbcf5eb00ed3a251ec46c99976de6845c
Sean Norwood <norwood.sean@gmail.com>
2021-10-17 18:37:37 -0500

    chore: update helm repo add command

--- scripts/helm-push.sh
+++ scripts/helm-push.sh
@@ -5,9 +5,12 @@ set -e
 CHART_VERSION=$(yq eval '.version' k8s/treats-portfolio/Chart.yaml)
 CHART_NAME=$(yq eval '.name' k8s/treats-portfolio/Chart.yaml)
 CHART_TARBALL="${CHART_NAME}"-"${CHART_VERSION}".tgz
+CHART_REPO_NAME=${CHART_NAME}
 
 echo "---Chart version: ${CHART_VERSION}---"
 echo "---Chart file: ${CHART_TARBALL}---"
+echo "---Chart file: ${CHART_REPO_URL}---"
+echo "---Chart file: ${CHART_REPO_NAME}---"
 
 helm repo add ${CHART_REPO_NAME} ${CHART_REPO_URL}
 
