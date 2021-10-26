0db86ff0f81a0a4976e79448890c1be3f58e272e
Sean Norwood <norwood.sean@gmail.com>
2021-10-26 15:14:38 -0500

    fix: update chart repo name

--- scripts/helm-push.sh
+++ scripts/helm-push.sh
@@ -5,7 +5,7 @@ set -e
 CHART_VERSION=$(yq eval '.version' k8s/treats-portfolio/Chart.yaml)
 CHART_NAME=$(yq eval '.name' k8s/treats-portfolio/Chart.yaml)
 CHART_TARBALL="${CHART_NAME}"-"${CHART_VERSION}".tgz
-CHART_REPO_NAME=${CHART_NAME}
+CHART_REPO_NAME="stiforr"
 
 echo "---Chart version: ${CHART_VERSION}---"
 echo "---Chart tarball: ${CHART_TARBALL}---"
